a5892c7f30e5d44969e83b7e641fdc4092a35863
Logan Meyers <theterrarian03@gmail.com>
2023-03-28 05:52:43 -0700

    Added a file that will become the file for encrypting the data

---
+++ aaaaaaa.py
@@ -0,0 +1,23 @@
+import numpy as np
+import wavio
+from matplotlib import pyplot as plt 
+import math
+import random
+
+
+def 
+
+def write_waves(frequencies: list, file_rate: int, note_time: float, show_graph: bool=True):
+    """
+    Takes in a list of frequencies for notes, and writes it out to a .wav file for listening or sharing
+    
+        Parameters
+            frequencies (list): list of note frequencies to write out, one after another
+            file_rate (int): sample rate of target .wav file
+            note_time (float): how long (in seconds) each note should play for
+            show_graph (boolean): whether to show a pyplot graph of waves
+    """
+
+
+
+write_waves()
