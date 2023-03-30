057684c0f17170f71a275a1ad39c1bf2af4cdf9f
Pk <105763979+pk-yomi@users.noreply.github.com>
2023-03-30 00:41:38 -0700

    Added UI and worked on finishing encoding process.

--- binary_conversion.py
+++ binary_conversion.py
@@ -1,8 +1,5 @@
 import library, tkinter as tk
 
-def get_user_message():
-    
-
 def binary_conversion(message):
     ascii_values = [] # List for ascii values
     binary_values = [] # List for final binary message
@@ -13,12 +10,36 @@ def binary_conversion(message):
     for value in ascii_values: # Turns ascii to binary
         binary_values.append(bin(value)[2:])
 
-    return binary_values 
+    return binary_values
 
-'''
 # Syncs binary to sound
-def sync_message(binary_values):
-    for binary_values in library.notes_and_letters
-'''
+def sync_message(message):
+    binary_inds = []
 
-print(binary_conversion('hello'))
+    for notes in library.notes_and_letters:
+        for values in library.notes_and_letters[notes]:
+            print(values)
+            if values in message:
+                print('woohoo') 
+                print('value:',values,'notes:',notes)
+                #binary_inds.append()
+                print(library.notes_and_letters[notes.index(values)])
+            else:
+                print('no')
+
+# Creates UI for inputing a message
+msg_window = tk.Tk() # Creates a window
+msg_window.wm_geometry('500x500')
+msg_window.title('Message Encoder') 
+msg_window.configure(background= '#B2D2A4')
+
+# Text box for user input
+msg = 'M'
+msg_box = tk.Entry(msg_window, textvariable=msg)
+msg_box.grid(column=6, row=8, sticky='news')
+
+# Enter button
+enter = tk.Button(msg_window, text='Enter Message', command= sync_message(binary_conversion(msg)))
+enter.grid(column=6, row=7, sticky='news')
+
+msg_window.mainloop()
