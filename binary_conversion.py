88b48f3cabad0517e4408c3adfe6aa4f67ac07b0
Pk <105763979+pk-yomi@users.noreply.github.com>
2023-04-08 20:53:57 -0700

    asdasdsd

--- binary_conversion.py
+++ binary_conversion.py
@@ -4,18 +4,21 @@ def binary_conversion(message):
     ascii_values = [] # List for ascii values
     binary_values = [] # List for final binary message
 
-    message_length = 0
-
     for letter in message: # Iterates through string and applies ascii value
         ascii_values.append(ord(letter))
 
     for value in ascii_values: # Turns ascii to binary
         binary_values.append(bin(value)[2:])
 
+    global msg_length
+    msg_length = len(ascii_values)
+
     return binary_values
 
 # Syncs binary to sound
 def sync_message(message):
+ 
+
     all_notes = [] # Each individual letters notes to sort through
     binary_inds = [] # Indexes of the letters in notes_and_letters
 
@@ -30,8 +33,10 @@ def sync_message(message):
     
     for name, index in zip(all_notes, binary_inds): # Creates tuples from the lists named
         frequencies.append(library.frequencies[name][index]) # Matches the list name with the needed index and stores it!
-    
-    return frequencies     
+
+
+    print('all notes', all_notes)
+    return print(frequencies)
 
 # Function for button press
 def button_ui():
