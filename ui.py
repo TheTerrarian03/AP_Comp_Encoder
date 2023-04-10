37214ca7aec2e684c86ed6bb34f321edf5a898e3
Pk <105763979+pk-yomi@users.noreply.github.com>
2023-04-09 19:47:06 -0700

    Changed the UI a bit

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-import tkinter as tk, binary_conversion
+import tkinter as tk, binary_conversion, decode
 from tkinter import ttk
 from tkinter import filedialog
 
@@ -16,21 +16,21 @@ decoding_page.grid(column=0, row=0, sticky='news')
 encoding_page = tk.Frame(root)
 encoding_page.grid(column= 0, row=0, sticky='news')
 
-def back_buttons():
+
+def file_decoding():
+    return(decode.decode(file_name))
+
+def back_buttons(): # Sends user back to start page
     return(start_page.tkraise())
 
 # Allows user to select file
 def browse_files():
-    filename = filedialog.askopenfilename(initialdir='/',title ='Select a file',filetypes=(('Wavio files','*.wav*'),('all files', '*.*'))) # Should open file explorer
+    filename = filedialog.askopenfilename(initialdir='/',title ='Select a file',filetypes=(('Wave Files','*.wav*'),('All Files', '*.*'))) # Should open file explorer
     file_name.configure(text = filename) # Changes label so user can see the file they selected
 
 # Function for enter button on encoding page
 def encoding_button():
     return(binary_conversion.sync_message(binary_conversion.message_to_binary(msg.get())))
-
-# Function for button on start page
-def enter_button():
-    return(options.bind('<<ComboboxSelected>>', options_changed)) # Calls function to match combobox option selected to it's page
 
 # Function for dropdown menu on start page
 def options_changed(event):
@@ -49,8 +49,7 @@ options = ttk.Combobox(start_page, values=options_available, state ='readonly') 
 options.current(0) # Sets default value to none
 options.grid(column=4,row=1, sticky = 'news') # Places dropdown menu
 
-enter_choice = tk.Button(start_page, text = 'Goto', command = enter_button) # Checks value in dropdown menu
-enter_choice.grid(column=4, row=2, sticky = 'news')
+options.bind('<<ComboboxSelected>>', options_changed) # Matches combobox option selected to it's page
 
 # - Decoding Page -
 file_name = tk.Label(decoding_page, text = '(file name here)') # Label for file name
@@ -58,6 +57,9 @@ file_name.grid(column=4,row=1,sticky= 'news')
 
 file_select = tk.Button(decoding_page, text = 'Select file', command= browse_files) # Button for selecting file
 file_select.grid(column=4, row=2, sticky='news')
+
+enter_file = tk.Button(decoding_page, text ='Enter', command= file_decoding) # Button for sending file to decoding
+enter_file.grid(column=4,row=3)
 
 back_button1 = tk.Button(decoding_page, text ='Back', command=back_buttons) # Takes you back to start page
 back_button1.grid(column=4, row=4)
